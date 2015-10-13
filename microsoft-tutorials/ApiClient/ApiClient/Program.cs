a2464b3ff6fcb96b7cdad381cad941a0ddef2b83
MattPuzey <mpuzey1@outlook.com>
2015-10-13 12:15:43 +0100

    API client

---
+++ microsoft-tutorials/ApiClient/ApiClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAync().Wait();
+        }
+
+        static async Task RunAync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:9000/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // HTTP GET:
+                HttpResponseMessage response = await client.GetAsync("api/products/1");
+                if (response.IsSuccessStatusCode)
+                {
+                    Product product = await response.Content.ReadAsAsync<Product>();
+                    Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
+                }
+
+
+                 //HTTP POST:
+                var gizmo = new Product() { Name = "Gizmo", Price = 100, Category = "Widget" };
+                response = await client.PostAsJsonAsync("api/products", gizmo);
+                if (response.IsSuccessStatusCode)
+                {
+                    // Get the URI of the created resource.
+                    Uri gizmoUrl = response.Headers.Location;
+
+                    // HTTP PUT
+                    gizmo.Price = 80;   // Update price
+                    response = await client.PutAsJsonAsync(gizmoUrl, gizmo);
+
+                    // HTTP DELETE
+                    response = await client.DeleteAsync(gizmoUrl);
+                }
+
+
+            }
+        }
+    }
+}
